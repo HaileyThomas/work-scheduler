b2ba992cfc7933b2788035a1a6b925ae4cb3d27a
Hailey Thomas <haileyraebeauty@gmail.com>
2021-10-11 12:42:38 -0700

    make list

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,28 @@ var currentDate = moment().format("dddd, MMMM Do YYYY");
 // add date to p element
 var showDate = $("#currentDay").text(currentDate);
 
+
+var editedTasks = JSON.parse(localStorage.getItem("tasks"));
+
+
 // change task
-$(".list-group").on("click","p", function () {
-    console.log("<p> was clicked");
+$(".list-group").on("click", "p", function () {
+    var text = $(this)
+        .text()
+        .trim();
+    var textInput = $("<textarea>")
+        .addClass("form-control")
+        .val(text);
+    // change to text area when clicked
+    $(this).replaceWith(textInput);
+    textInput.trigger("focus");
+    // save task
+    $(".list-group").on("click", "span", function () {
+        console.log("save button was clicked");
+    });
 });
+
+// save tasks
+var saveTasks = function () {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+};
