b03d5e167ac1f538ce886993264c0d83b12db6ab
Hailey Thomas <haileyraebeauty@gmail.com>
2021-10-11 11:18:13 -0700

    work on layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,8 @@
 var currentDate = moment().format("dddd, MMMM Do YYYY");
 // add date to p element
 var showDate = $("#currentDay").text(currentDate);
+
+// change task
+$(".list-group").on("click","p", function () {
+    console.log("<p> was clicked");
+});
