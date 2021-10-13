64732e6728bb972a26bb1b22c884cf91b7bfb9a1
Hailey Thomas <haileyraebeauty@gmail.com>
2021-10-13 16:33:01 -0700

    work on colors and displaying array

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,16 +20,101 @@ var threeEl = taskArray[6];
 var fourEl = taskArray[7];
 var fiveEl = taskArray[8];
 
+console.log(nineEl);
+
 // DISPLAY CURRENT DATE
 var currentDate = moment().format("dddd, MMMM Do YYYY");
 // add date to p element
 var showDate = $("#currentDay").text(currentDate);
 
 // TIME
-var currentTime = moment().hour();
-console.log(currentTime);
+var currentTime = moment();
+var eightAm = moment().hour(8);
+var nineAm = moment().hour(9);
+var tenAm = moment().hour(10);
+var elevenAm = moment().hour(11);
+var twelvePm = moment().hour(12);
+var onePm = moment().hour(13);
+var twoPm = moment().hour(14);
+var threePm = moment().hour(15);
+var fourPm = moment().hour(16);
+var fivePm = moment().hour(17);
+var sixPm = moment().hour(18);
+if (currentTime.isBetween(eightAm, tenAm)) {
+    $("#nine").addClass("bg-danger");
+    $("#ten").addClass("bg-success");
+    $("#eleven").addClass("bg-success");
+    $("#twelve").addClass("bg-success");
+    $("#one").addClass("bg-success");
+    $("#two").addClass("bg-success");
+    $("#three").addClass("bg-success");
+    $("#four").addClass("bg-success");
+    $("#five").addClass("bg-success");
+};
+if (currentTime.isBetween(nineAm, elevenAm)) {
+    $("#ten").addClass("bg-danger");
+    $("#eleven").addClass("bg-success");
+    $("#twelve").addClass("bg-success");
+    $("#one").addClass("bg-success");
+    $("#two").addClass("bg-success");
+    $("#three").addClass("bg-success");
+    $("#four").addClass("bg-success");
+    $("#five").addClass("bg-success");
+};
+if (currentTime.isBetween(tenAm, twelvePm)) {
+    $("#eleven").addClass("bg-danger");
+    $("#twelve").addClass("bg-success");
+    $("#one").addClass("bg-success");
+    $("#two").addClass("bg-success");
+    $("#three").addClass("bg-success");
+    $("#four").addClass("bg-success");
+    $("#five").addClass("bg-success");
+};
+if (currentTime.isBetween(elevenAm, onePm)) {
+    $("#twelve").addClass("bg-danger");
+    $("#one").addClass("bg-success");
+    $("#two").addClass("bg-success");
+    $("#three").addClass("bg-success");
+    $("#four").addClass("bg-success");
+    $("#five").addClass("bg-success");
+};
+if (currentTime.isBetween(twelvePm, twoPm)) {
+    $("#one").addClass("bg-danger");
+    $("#two").addClass("bg-success");
+    $("#three").addClass("bg-success");
+    $("#four").addClass("bg-success");
+    $("#five").addClass("bg-success");
+};
+if (currentTime.isBetween(onePm, threePm)) {
+    $("#two").addClass("bg-danger");
+    $("#three").addClass("bg-success");
+    $("#four").addClass("bg-success");
+    $("#five").addClass("bg-success");
+};
+if (currentTime.isBetween(twoPm, fourPm)) {
+    $("#three").addClass("bg-danger");
+    $("#four").addClass("bg-success");
+    $("#five").addClass("bg-success");
+}
+if (currentTime.isBetween(threePm, fivePm)) {
+    $("#four").addClass("bg-danger");
+    $("#five").addClass("bg-success");
+};
+if (currentTime.isBetween(fourPm, sixPm)) {
+    $("#five").addClass("bg-success");
+};
+
 
 // LOAD TASKS
+$("#nine-text").text(nineEl);
+$("#ten-text").text(tenEl);
+$("#eleven-text").text(elevenEl);
+$("#twelve-text").text(twelveEl);
+$("#one-text").text(oneEl);
+$("#two-text").text(twoEl);
+$("#three-text").text(threeEl);
+$("#four-text").text(fourEl);
+$("#five-text").text(fiveEl);
 
 
 // EDIT TASK WHEN CLICKED
