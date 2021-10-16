a29bbf4b20a7d8c82edc56c2f0de3b809cf48fad
Hailey Thomas <haileyraebeauty@gmail.com>
2021-10-16 15:04:12 -0700

    save tasks to localstorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,59 +20,59 @@ var fourPm = moment().hour(16);
 var fivePm = moment().hour(17);
 var sixPm = moment().hour(18);
 if (currentTime.isBetween(eightAm, tenAm)) {
-    $("#nine").addClass("bg-danger");
-    $("#ten").addClass("bg-success");
-    $("#eleven").addClass("bg-success");
-    $("#twelve").addClass("bg-success");
-    $("#one").addClass("bg-success");
-    $("#two").addClass("bg-success");
-    $("#three").addClass("bg-success");
-    $("#four").addClass("bg-success");
-    $("#five").addClass("bg-success");
+    $("#nine-div").addClass("bg-danger");
+    $("#ten-div").addClass("bg-success");
+    $("#eleven-div").addClass("bg-success");
+    $("#twelve-div").addClass("bg-success");
+    $("#one-div").addClass("bg-success");
+    $("#two-div").addClass("bg-success");
+    $("#three-div").addClass("bg-success");
+    $("#four-div").addClass("bg-success");
+    $("#five-div").addClass("bg-success");
 } else if (currentTime.isBetween(nineAm, elevenAm)) {
-    $("#ten").addClass("bg-danger");
-    $("#eleven").addClass("bg-success");
-    $("#twelve").addClass("bg-success");
-    $("#one").addClass("bg-success");
-    $("#two").addClass("bg-success");
-    $("#three").addClass("bg-success");
-    $("#four").addClass("bg-success");
-    $("#five").addClass("bg-success");
+    $("#ten-div").addClass("bg-danger");
+    $("#eleven-div").addClass("bg-success");
+    $("#twelve-div").addClass("bg-success");
+    $("#one-div").addClass("bg-success");
+    $("#two-div").addClass("bg-success");
+    $("#three-div").addClass("bg-success");
+    $("#four-div").addClass("bg-success");
+    $("#five-div").addClass("bg-success");
 } else if (currentTime.isBetween(tenAm, twelvePm)) {
-    $("#eleven").addClass("bg-danger");
-    $("#twelve").addClass("bg-success");
-    $("#one").addClass("bg-success");
-    $("#two").addClass("bg-success");
-    $("#three").addClass("bg-success");
-    $("#four").addClass("bg-success");
-    $("#five").addClass("bg-success");
+    $("#eleven-div").addClass("bg-danger");
+    $("#twelve-div").addClass("bg-success");
+    $("#one-div").addClass("bg-success");
+    $("#two-div").addClass("bg-success");
+    $("#three-div").addClass("bg-success");
+    $("#four-div").addClass("bg-success");
+    $("#five-div").addClass("bg-success");
 } else if (currentTime.isBetween(elevenAm, onePm)) {
-    $("#twelve").addClass("bg-danger");
-    $("#one").addClass("bg-success");
-    $("#two").addClass("bg-success");
-    $("#three").addClass("bg-success");
-    $("#four").addClass("bg-success");
-    $("#five").addClass("bg-success");
+    $("#twelve-div").addClass("bg-danger");
+    $("#one-div").addClass("bg-success");
+    $("#two-div").addClass("bg-success");
+    $("#three-div").addClass("bg-success");
+    $("#four-div").addClass("bg-success");
+    $("#five-div").addClass("bg-success");
 } else if (currentTime.isBetween(twelvePm, twoPm)) {
-    $("#one").addClass("bg-danger");
-    $("#two").addClass("bg-success");
-    $("#three").addClass("bg-success");
-    $("#four").addClass("bg-success");
-    $("#five").addClass("bg-success");
+    $("#one-div").addClass("bg-danger");
+    $("#two-div").addClass("bg-success");
+    $("#three-div").addClass("bg-success");
+    $("#four-div").addClass("bg-success");
+    $("#five-div").addClass("bg-success");
 } else if (currentTime.isBetween(onePm, threePm)) {
-    $("#two").addClass("bg-danger");
-    $("#three").addClass("bg-success");
-    $("#four").addClass("bg-success");
-    $("#five").addClass("bg-success");
+    $("#two-div").addClass("bg-danger");
+    $("#three-div").addClass("bg-success");
+    $("#four-div").addClass("bg-success");
+    $("#five-div").addClass("bg-success");
 } else if (currentTime.isBetween(twoPm, fourPm)) {
-    $("#three").addClass("bg-danger");
-    $("#four").addClass("bg-success");
-    $("#five").addClass("bg-success");
+    $("#three-div").addClass("bg-danger");
+    $("#four-div").addClass("bg-success");
+    $("#five-div").addClass("bg-success");
 } else if (currentTime.isBetween(threePm, fivePm)) {
-    $("#four").addClass("bg-danger");
-    $("#five").addClass("bg-success");
+    $("#four-div").addClass("bg-danger");
+    $("#five-div").addClass("bg-success");
 } else if (currentTime.isBetween(fourPm, sixPm)) {
-    $("#five").addClass("bg-danger");
+    $("#five-div").addClass("bg-danger");
 };
 
 // LOAD TASKS FUNCTION
@@ -82,6 +82,47 @@ var loadTasks = function () {
     } else {
         $("#nine-text").text("There are currently no tasks.");
     };
+    if ("ten" in localStorage) {
+        $("#ten-text").text(localStorage.getItem("ten"));
+    } else {
+        $("#ten-text").text("There are currently no tasks.");
+    };
+    if ("eleven" in localStorage) {
+        $("#eleven-text").text(localStorage.getItem("eleven"));
+    } else {
+        $("#eleven-text").text("There are currently no tasks.");
+    };
+    if ("twelve" in localStorage) {
+        $("#twelve-text").text(localStorage.getItem("twelve"));
+    } else {
+        $("#twelve-text").text("There are currently no tasks.");
+    };
+    if ("one" in localStorage) {
+        $("#one-text").text(localStorage.getItem("one"));
+    } else {
+        $("#one-text").text("There are currently no tasks.");
+    };
+    if ("two" in localStorage) {
+        $("#two-text").text(localStorage.getItem("two"));
+    } else {
+        $("#two-text").text("There are currently no tasks.");
+    };
+    if ("three" in localStorage) {
+        $("#three-text").text(localStorage.getItem("three"));
+    } else {
+        $("#three-text").text("There are currently no tasks.");
+    };
+    if ("four" in localStorage) {
+        $("#four-text").text(localStorage.getItem("four"));
+    } else {
+        $("#four-text").text("There are currently no tasks.");
+    };
+    if ("five" in localStorage) {
+        $("#five-text").text(localStorage.getItem("five"));
+    } else {
+        $("#five-text").text("There are currently no tasks.");
+    };
+
 };
 
 // CALL LOAD TASKS FUNCTION
@@ -109,9 +150,11 @@ $(".list-group").on("click", "p", function () {
         var text = $(textInput)
             .val()
             .trim();
-        var index = $(textInput)
-            .closest(".list-group-item")
-            .index();
+        var index = $(this)
+            .parent()
+            .attr("id");
+        console.log(text);
+        console.log(index);
         console.log("index number: " + index);
         // create new p element with new text
         taskP = $("<p>").text(text);
@@ -121,16 +164,8 @@ $(".list-group").on("click", "p", function () {
         console.log("text: " + text);
         editedTask = text;
         console.log("editedTask: " + editedTask);
-        saveTask();
+        localStorage.setItem(index, text);
     });
 });
 
-// SAVE TASKS FUNCTION
-var saveTask = function () {
-    if (taskP.parent("#nine")) {
-        localStorage.setItem("nine", JSON.stringify(editedTask));
-    };
-    if (taskP.parent("#ten")) {
-        localStorage.setItem("ten", JSON.stringify(editedTask));
-    };
-}
+
